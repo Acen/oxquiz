10a0f6ec3e5c16842b8b0454e238ef199d2cdcc0
Zac Bruce <zac@tuft.co.nz>
2018-09-27 20:51:36 +1200

    Add some quality about text. #originalcontent

--- resources/assets/js/components/page/About.vue
+++ resources/assets/js/components/page/About.vue
@@ -1,7 +1,21 @@
 <template>
     <el-row type="flex" justify="center">
         <el-col :span="18" :xs="24">
-            <p>Hi Mom</p>
+            <h3>Oh, hi there!</h3>
+            <p>This OX Quiz helper service has been created so that we can win together.</p>
+            <h3>Watchu mean?</h3>
+            <p>Practically, Maplestory 2 has a mini-game available based around the infamous true/false style GameMaster event that Maplestory 1 had.</p>
+            <p>The players get asked a question, and then there's a pretty simple true or false answer required. The problem though, is that the less knowledgeable of us, don't know the answers. And the questions have been written in a way that makes them insane-hard to Google.</p>
+            <p>One of the nice benefits of this system that Nexon has made, is that even if every single player wins, there aren't any terrible losses or negative effects for anyone. More winners is just better for everyone.</p>
+            <p>And thus, this derpy service was born. We crowdsource the questions and answers. We win together.</p>
+            <h3>What's in it for you, random author of website!? Trying to hack monies!??!??!</h3>
+            <p>I'm just a dirty PHP developer that works in Laravel and VueJS. There's a cool part of this website in terms of developmentness that uses a Vue feature called VueX. If you've done any React development, then it's a similar system to Redux.</p>
+            <p>I decided that this would be a great opportunity to have some open source code that promotes my ability to build things (paid work is usually closed :P), while also providing a handy service to one of the communities I'm a part of.</p>
+            <h3><em>[SOMETHING]</em> is broken!</h3>
+            <p>Frick, that sucks.</p>
+            <p>First off, see if it can be replicated. If so, write them steps down fam. Makes whoever ends up maintaining the project easier to debug problems.</p>
+            <p>Submit an Issue to our <a href="https://github.com/Acen/oxquiz" target="_blank">Github page</a>. If you have any questions that can be made public, ask them there too.</p>
+            <p>If it's something you want to be more private, then hit me up on <em>zac[at]tuft[dot]co[dot]nz</em>. Have a great day.</p>
         </el-col>
     </el-row>
 </template>
