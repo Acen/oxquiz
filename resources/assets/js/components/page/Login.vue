b97cfd96eca0c3a4c38cc826c5892df52e90495b
Zac Bruce <zac@tuft.co.nz>
2018-09-27 20:48:49 +1200

    Clean up that sending of logn/rego credentails

--- resources/assets/js/components/page/Login.vue
+++ resources/assets/js/components/page/Login.vue
@@ -4,14 +4,14 @@
             <el-form>
                 <el-form-item>
                     <label for="email">Email</label>
-                    <el-input id="email" v-model="email" type="email" placeholder="email@example.com"></el-input>
+                    <el-input id="email" v-model="user.email" type="email" placeholder="email@example.com"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <label for="password">Password</label>
-                    <el-input id="password" v-model="password" type="password"></el-input>
+                    <el-input id="password" v-model="user.password" type="password"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button>Login</el-button>
+                    <el-button @click="submit()" :loading="user.saving">Login</el-button>
                 </el-form-item>
             </el-form>
         </el-col>
@@ -19,14 +19,35 @@
 </template>
 
 <script>
+    import Login from '../../models/Login';
+    import { mapActions } from 'vuex';
+
     export default {
         name: "Login",
         data() {
             return {
-                'email': '',
-                'password': '',
+                user: new Login(),
             };
         },
+        methods: {
+            submit() {
+                this.user.save()
+                    .then(() => {
+                        // Login User
+                        this.login(this.user)
+                            .then(() => {
+                                // Redirect back to home.
+                                this.$router.push({name: 'search'});
+                            });
+                    })
+                    .catch(( error ) => {
+                        console.error('login.save', error);
+                    });
+            },
+            ...mapActions([
+                'login',
+            ]),
+        }
     };
 </script>
 
